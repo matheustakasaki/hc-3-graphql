5e6f3d7c63df7373aa97ae791429f09b980b476b
matheustakasaki <matheus8antunes@gmail.com>
2022-07-09 22:21:06 -0300

    Montando resposta do servidor

--- client-server/src/main.js
+++ client-server/src/main.js
@@ -1,1 +1,18 @@
 import { createServer } from 'http'
+
+const server = createServer((req, res) => {
+    switch (req.url) {
+        case '/status': {
+            res.writeHead(200);
+            res.write('okay');
+            res.end();
+
+            return;
+        }
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Server está rodando em http://127.0.0.1');
+
+})
