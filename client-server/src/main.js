18f1ab709f3aa0d089404f0334883705c43df1ad
matheustakasaki <matheus8antunes@gmail.com>
2022-07-13 18:24:29 -0300

    Configurando servidor e setando resposta default para rotas diferentes de /status

--- client-server/src/main.js
+++ client-server/src/main.js
@@ -3,16 +3,31 @@ import { createServer } from 'http'
 const server = createServer((req, res) => {
     switch (req.url) {
         case '/status': {
-            res.writeHead(200);
-            res.write('okay');
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            res.write(JSON.stringify({
+                status: 'okay'
+            }));
             res.end();
 
             return;
         }
+
+
+        // Qualquer coisa que não for /status resultará em 404
+        default: {
+            res.writeHead(404, 'Service not found');
+            res.end();
+        }
     }
 });
 
-server.listen(8000, '127.0.0.1', () => {
-    console.log('Server está rodando em http://127.0.0.1');
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1'
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server está rodando em http://${HOSTNAME}:${PORT}`);
 
 })
